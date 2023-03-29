a32a418aca6faf2e4eff66be921db89993dc489f
alaaahmade <alaaahmade66@gmail.com>
2023-03-29 15:19:08 +0200

    testing server endpointes Reltes #8

---
+++ src/tests/routesFilter.test.js
@@ -0,0 +1,85 @@
+const request = require("supertest")
+const app = require('../app');
+
+const {
+    getAllEmojis,
+    getSearchedEmojis,
+    getGroupes,
+    getCatogry,
+  } = require('../controllers/index');
+
+test('/groups/body  expect status to be 200' , (done) => {
+    request(app)
+       .get('/groups/body',getGroupes)
+       .expect(200)
+       .end((err, res) =>{
+           if(err)return done(err)
+       expect(res.statusCode).toBe(200)
+    //    expect(typeof(res.body)).toEqual();
+       return done()
+       })
+   })
+   test('/grops/body  expect status to be 404' , (done) => {
+    request(app)
+       .get('/grops/body',getGroupes)
+       .expect(404)
+       .end((err, res) =>{
+           if(err)return done(err)
+       expect(res.statusCode).toBe(404)
+       return done()
+       })
+   })
+   test('/categy/:category  expect status to be 404' , (done) => {
+    request(app)
+       .get('/catery/:category',getGroupes)
+       .expect(404)
+       .end((err, res) =>{
+           if(err)return done(err)
+       expect(res.statusCode).toBe(404)
+       return done()
+       })
+   })
+   test('/category/:category  expect status to be 404' , (done) => {
+    request(app)
+       .get('/category/:category',getGroupes)
+       .expect(200)
+       .end((err, res) =>{
+           if(err)return done(err)
+       expect(res.statusCode).toBe(200)
+       return done()
+       })
+   })
+   test('/category/:category  expect status to be 200' , (done) => {
+    request(app)
+       .get('/category/flags',getCatogry)
+       .expect(200)
+       .end((err, res) =>{
+           if(err) return done(err)
+
+            expect(Array.isArray(res.body)).toBe(true);
+            return done()
+       })
+   })
+   
+   test('/category/:category  expect status to be 200' , (done) => {
+    request(app)
+       .get('/category/flags',getCatogry)
+       .expect(200)
+       .end((err, res) =>{
+           if(err) return done(err)
+
+            expect(typeof res.body[0]).toBe('object');
+            return done()
+       })
+   })
+     
+   test('/cegory/:category  expect status to be 404' , (done) => {
+    request(app)
+       .get('/cegory/flags',getCatogry)
+       .expect(404)
+       .end((err, res) =>{
+           if(err) return done(err)
+            expect(res.statusCode).toBe(404)
+            return done()
+       })
+   })
