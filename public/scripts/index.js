ec9fc18d3869886c16a18dd217bf2a9dd9798c8f
alaaahmade <alaaahmade66@gmail.com>
2023-03-29 12:46:47 +0200

    Create Filters requists And Function From Bacend Relates #4

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,18 @@
+/* eslint-disable no-undef */
 const category = document.getElementById('category');
 const groups = document.getElementById('groups');
 const filterBtn = document.querySelector('.filter_button');
 
-// const fetchFunc = (url, opchens) => fetch(url, opchens).then((data) => data.json());
 filterBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (category.value && groups.value) {
-    fetchFunc(`/groups/${groups.value}`);
+    fetchFunc(`/groups/${groups.value}`)
+      .then(console.log);
   } else if (category.value) {
-    fetchFunc(`/category/${category.value}`);
+    fetchFunc(`/category/${category.value}`)
+      .then(console.log);
   } else if (groups.value) {
-    fetchFunc(`/groups/${groups.value}`);
+    fetchFunc(`/groups/${groups.value}`)
+      .then(console.log);
   }
 });
